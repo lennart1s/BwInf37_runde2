cf73af5d7d7c2dfac9ab0ff7897665542e40f0a4
lennart1s <sandbothe.lennart@gmail.com>
2019-03-09 19:25:03 +0100

    Clean up

--- lisa_rennt/LisaRennt.go
+++ lisa_rennt/LisaRennt.go
@@ -1,21 +1,19 @@
 package main
 
 import (
+	"BwInf37_runde2/lisa_rennt/file"
 	"BwInf37_runde2/lisa_rennt/rendering"
 	"image"
 	"image/draw"
 	"image/png"
-	"io/ioutil"
 	"os"
-	"strconv"
-	"strings"
 
 	"github.com/llgcode/draw2d/draw2dimg"
 )
 
 var (
-	obstacles []*rendering.Polygon
-	home      rendering.Vertex
+	Obstacles []*rendering.Polygon
+	Home      rendering.Vertex
 )
 
 func main() {
@@ -27,48 +25,12 @@ func main() {
 	gc.Translate(rendering.BUSSTOP_RADIUS*1.5, -(float64(rendering.HEIGHT) - rendering.BUSSTOP_RADIUS*1.5))
 
 	// Read-File
-	readFile("./lisa_rennt/examples/lisarennt2.txt")
+	Obstacles, Home = file.Read("./lisa_rennt/examples/lisarennt5.txt")
 
 	rendering.RenderEnvironment(gc)
-	rendering.RenderHome(gc, home)
-	rendering.RenderObstacles(gc, obstacles...)
+	rendering.RenderHome(gc, Home)
+	rendering.RenderObstacles(gc, Obstacles...)
 
-	f, _ := os.Create("save.png")
+	f, _ := os.Create("./lisa_rennt/save.png")
 	png.Encode(f, img)
-}
-
-func readFile(path string) {
-	lines := loadLines(path)
-	numPolygons, err := strconv.Atoi(lines[0])
-	for _, line := range lines[1 : 1+numPolygons] {
-		obstacles = append(obstacles, rendering.PolygonFromLine(line))
-	}
-
-	homeParts := strings.Split(lines[len(lines)-1], " ")
-	home.X, err = strconv.ParseFloat(homeParts[0], 64)
-	home.Y, err = strconv.ParseFloat(homeParts[1], 64)
-
-	if err != nil {
-		panic("Invalid data in file: " + path)
-	}
-}
-
-func loadText(path string) string {
-	data, err := ioutil.ReadFile(path)
-	if err != nil {
-		panic(err)
-	}
-
-	return string(data)
-}
-
-func loadLines(path string) []string {
-	text := loadText(path)
-	var lines []string
-	for _, lineA := range strings.Split(text, "\r\n") {
-		for _, lineB := range strings.Split(lineA, "\n") {
-			lines = append(lines, lineB)
-		}
-	}
-	return lines
 }
---
+++ lisa_rennt/file/ReadFile.go
@@ -0,0 +1,50 @@
+package file
+
+import (
+	"BwInf37_runde2/lisa_rennt/rendering"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func Read(path string) ([]*rendering.Polygon, rendering.Vertex) {
+	var obstacles []*rendering.Polygon
+	var home rendering.Vertex
+
+	lines := loadLines(path)
+	numPolygons, err := strconv.Atoi(lines[0])
+	for _, line := range lines[1 : 1+numPolygons] {
+		obstacles = append(obstacles, rendering.PolygonFromLine(line))
+	}
+
+	homeParts := strings.Split(lines[len(lines)-1], " ")
+	home.X, err = strconv.ParseFloat(homeParts[0], 64)
+	home.Y, err = strconv.ParseFloat(homeParts[1], 64)
+
+	if err != nil {
+		panic("Invalid data in file: " + path)
+		return nil, rendering.Vertex{}
+	}
+
+	return obstacles, home
+}
+
+func loadText(path string) string {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(data)
+}
+
+func loadLines(path string) []string {
+	text := loadText(path)
+	var lines []string
+	for _, lineA := range strings.Split(text, "\r\n") {
+		for _, lineB := range strings.Split(lineA, "\n") {
+			lines = append(lines, lineB)
+		}
+	}
+	return lines
+}
