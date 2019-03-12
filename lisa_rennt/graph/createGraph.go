b956894c0e6b6fafac766b7973bc85ced9b18111
lennart1s <sandbothe.lennart@gmail.com>
2019-03-12 16:23:58 +0100

    Inter-Polygonal Graph-Creation Working

--- lisa_rennt/graph/createGraph.go
+++ lisa_rennt/graph/createGraph.go
@@ -3,86 +3,40 @@ package graph
 import (
 	"BwInf37_runde2/lisa_rennt/lib"
 	"math"
+	"strconv"
+	"strings"
 )
 
 func Create(home lib.Vertex, polygons []*lib.Polygon) lib.Graph {
 	g := lib.Graph{}
-	//g.Nodes = append(g.Nodes, &lib.Node{X: home.X, Y: home.Y})
-	//g.Nodes = append(g.Nodes, verticesToNodes(polygons)...)
+	g.Nodes = append(g.Nodes, &lib.Node{X: home.X, Y: home.Y})
+	g.Nodes = append(g.Nodes, verticesToNodes(polygons)...)
 
-	//var nodes map[*lib.Vertex]*lib.Node
-	nodes := make(map[lib.Vertex]*lib.Node)
-	nodes[lib.Vertex{}] = &lib.Node{X: home.X, Y: home.Y}
-	addAllVertices(&nodes, polygons)
+	var borders []*lib.Line
+	borders = append(borders, getAllLines(polygons)...)
 
-	var lines []*lib.Line
-	lines = append(lines, getAllLines(polygons)...)
-
-	
-
-	for _, p := range polygons {
-		for _, v := range p.Vertices {
-			for _, op := range polygons {
-				if op != p {
-					for _, ov := range op.Vertices {
-						if canReach(&v, &ov, lines) {
-							nodes[v].Edges = append(nodes[v].Edges, &lib.Edge{Node: nodes[ov], Weight: distance(&v, &ov)})
-						}
-					}
-				}
-			}
-		}
-	}
-
-	for _, n := range nodes {
-		g.Nodes = append(g.Nodes, n)
-	}
-
-	/*for _, n := range g.Nodes {
+	for _, n := range g.Nodes {
 		for _, o := range g.Nodes {
-			if n != o && canReach(n, o, lines) {
-				n.Edges = append(n.Edges, &lib.Edge{Node: o, Weight: distance(n, o)})
-			}
-		}
-	}*/
-
-	/*for _, p := range polygons {
-		for _, v := range p.Vertices {
-			n := lib.Node{X: v.X, Y: v.Y}
-			for _, op := range polygons {
-				if op != p {
-					for _, ov := range op.Vertices {
-						if canReach(v, ov, lines) {
-							n.Edges = append(n.Edges, lib.Edge{})
-						}
-					}
+			if !belongToSamePolygon(n, o) {
+				if canReach(n, o, borders) {
+					n.Edges = append(n.Edges, &lib.Edge{Node: o, Weight: distance(n, o)})
 				}
 			}
-			g.Nodes = append(g.Nodes, &n)
 		}
-	}*/
+	}
 
 	return g
 }
 
-func canReach(n *lib.Vertex, o *lib.Vertex, lines []*lib.Line) bool {
-	ray := lib.Line{A: *n, B: *o}
+func canReach(n *lib.Node, o *lib.Node, borders []*lib.Line) bool {
+	ray := lib.Line{A: lib.Vertex{X: n.X, Y: n.Y}, B: lib.Vertex{X: o.X, Y: o.Y}}
 
-	for _, l := range lines {
-		if l.A.X == ray.A.X && l.A.Y == ray.A.Y {
-			continue
-		}
-		if l.B.X == ray.A.X && l.B.Y == ray.A.Y {
-			continue
-		}
-		if l.A.X == ray.B.X && l.A.Y == ray.B.Y {
-			continue
-		}
-		if l.B.X == ray.B.X && l.B.Y == ray.B.Y {
+	for _, b := range borders {
+		if segmentsHaveCommandPoint(&ray, b) {
 			continue
 		}
 
-		if lib.LineSegementIntersection(&ray, l) {
+		if lib.LineSegementIntersection(&ray, b) {
 			return false
 		}
 	}
@@ -90,8 +44,17 @@ func canReach(n *lib.Vertex, o *lib.Vertex, lines []*lib.Line) bool {
 	return true
 }
 
-func distance(n *lib.Vertex, o *lib.Vertex) float64 {
+func distance(n *lib.Node, o *lib.Node) float64 {
 	return math.Sqrt(math.Pow(n.X-o.X, 2) + math.Pow(n.Y-o.Y, 2))
+}
+
+func belongToSamePolygon(n *lib.Node, o *lib.Node) bool {
+	return strings.Split(n.Id, ",")[0] == strings.Split(o.Id, ",")[0]
+}
+
+func segmentsHaveCommandPoint(l1 *lib.Line, l2 *lib.Line) bool {
+	return l1.A == l2.A || l1.A == l2.B ||
+		l1.B == l2.A || l1.B == l2.B
 }
 
 func getAllLines(polygons []*lib.Polygon) []*lib.Line {
@@ -118,13 +81,14 @@ func addAllVertices(nodes *map[lib.Vertex]*lib.Node, polygons []*lib.Polygon) {
 	}
 }
 
-/*func verticesToNodes(polygons []*lib.Polygon) []*lib.Node {
+func verticesToNodes(polygons []*lib.Polygon) []*lib.Node {
 	var nodes []*lib.Node
-	for _, p := range polygons {
-		for _, v := range p.Vertices {
-			nodes = append(nodes, &lib.Node{X: v.X, Y: v.Y})
+	for ip, p := range polygons {
+		for iv, v := range p.Vertices {
+			id := strconv.Itoa(ip) + "," + strconv.Itoa(iv)
+			nodes = append(nodes, &lib.Node{X: v.X, Y: v.Y, Id: id})
 		}
 	}
 
 	return nodes
-}*/
+}
